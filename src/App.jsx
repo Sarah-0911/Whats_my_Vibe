0098329252a96a5988e60aa33df01b948189cbea
Sarah-0911 <sarah.tabet69@gmail.com>
2025-01-24 19:28:07 +0100

    add hero vinyl picture

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import SearchInput from "./components/SearchInput";
 function App () {
 
   return (
-    <div className="m-4">
-      <div className="text-center">
+    <div className="flex h-screen w-screen">
+      <div className="text-center flex-[2]">
         <h1 className='text-slate-800 uppercase font-manrope font-bold text-4xl tracking-wider'>Track Hunt</h1>
-        <p className="font-barlow italic text-4xl font-light">dive into the music</p>
+        <p className="font-barlow italic text-4xl font-light">dive into the music.</p>
+        <SearchInput />
       </div>
-      <SearchInput />
+      <div className="flex-[1] basis-0">
+        <img
+        className="h-full w-full object-cover"
+        src="/images/bg-vinyl.jpg"
+        alt="vinyl picture"
+        />
+      </div>
     </div>
   )
 }
