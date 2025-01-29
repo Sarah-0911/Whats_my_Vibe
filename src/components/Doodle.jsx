309c48b833a587cd6ee69b6a213744d667f5c7a6
Sarah-0911 <sarah.tabet69@gmail.com>
2025-01-29 15:24:53 +0100

    musicDoodle ok for desktop

--- src/components/Doodle.jsx
+++ src/components/Doodle.jsx
@@ -3,15 +3,18 @@ import { audioTape, djembe, eighthNote, eighthNotes, note, guitar, harmonica, he
 export default function Doodle() {
   return (
     <>
-      <img className="absolute -top-40 left-0 w-80 h-auto" src={audioTape} alt="" />
-      <img className="absolute top-40 left-20 w-80 h-auto" src={djembe} alt="" />
-      <img className="absolute top-40 right-0 w-80 h-auto" src={guitar} alt="" />
-      <img className="absolute -top-20 right-20 w-80 h-auto" src={harmonica} alt="" />
-      <img className="absolute bottom-40 left-10 w-80 h-auto" src={headphones} alt="" />
-      <img className="absolute -bottom-20 right-60 w-80 h-auto" src={mike} alt="" />
-      <img className="absolute top-20 left-80 w-80 h-auto" src={recordPlayer} alt="" />
-      <img className="absolute bottom-0 left-0 w-80 h-auto" src={sticks} alt="" />
-      <img className="absolute -bottom-20 left-20 w-80 h-auto" src={tuningFork} alt="" />
+      <img className="absolute bottom-10 left-72 w-20 h-auto rotate-12" src={audioTape} alt="audioTape icon" />
+      <img className="absolute bottom-10 left-28 w-28 h-auto -rotate-12" src={djembe} alt="djembe icon" />
+      <img className="absolute bottom-12 right-12 w-24 h-auto rotate-12" src={guitar} alt="guitar icon" />
+      <img className="absolute top-24 left-1/3 w-20 h-auto" src={harmonica} alt="harmonica icon" />
+      <img className="absolute top-6 right-1/3 w-20 h-auto" src={headphones} alt="headphones icon" />
+      <img className="absolute top-20 left-14 w-16 h-auto rotate-12" src={mike} alt="mike icon" />
+      <img className="absolute top-20 right-14 w-32 h-auto" src={recordPlayer} alt="recordPlayer icon" />
+      <img className="absolute bottom-1/3 left-20 w-8 h-auto rotate-45" src={sticks} alt="sticks icon" />
+      <img className="absolute bottom-16 right-1/4 w-16 h-auto" src={tuningFork} alt="tuningFork icon" />
+      <img className="absolute bottom-6 right-96 w-6 h-auto rotate-12" src={eighthNote} alt="eighthNote icon" />
+      <img className="absolute top-12 right-10 w-8 h-auto" src={eighthNotes} alt="eighthNotes icon" />
+      <img className="absolute top-16 left-72 w-4 h-auto rotate-12" src={note} alt="note icon" />
     </>
   )
 }
