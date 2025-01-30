e451747c120e6b75185fcd8afdcd895820db3440
Sarah-0911 <sarah.tabet69@gmail.com>
2025-01-30 15:20:03 +0100

    update color saturation

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App () {
   const { apiData } = useContext(UserContext);
 
   return (
-    <div className="bg-slate-900">
-      <div className="flex flex-col justify-center h-screen pt-28 px-6 bg-[#e95d41] text-center shadow-[0_0_15px_5px_#000000]">
+    <div className="bg-slate-800">
+      <div className="flex flex-col justify-center h-screen pt-28 px-6 bg-[#dd634b] text-center shadow-[0_0_15px_5px_#000000]">
         <h1 className='text-slate-900 font-medium font-limelight uppercase text-5xl md:text-[82px] drop-shadow-lg'>
           What&apos;s my vibe
         </h1>
-        <p className="mt-1 font-poiret font-medium text-lg md:text-4xl drop-shadow-sm tracking-wider text-orange-200">
+        <p className="mt-1 font-poiret font-semibold text-lg md:text-4xl drop-shadow-sm tracking-wider text-orange-200">
           generate tracks from your words.
         </p>
         <SearchInput />
@@ -26,5 +26,3 @@ function App () {
 }
 
 export default App
-
-// bg-[url('/images/abstract.jpg')] bg-cover (2e div)
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -32,7 +32,7 @@ export default function SearchInput() {
       <button
         className="mt-4 rounded-full z-50 text-xl font-semibold  lowercase font-manrope px-8 py-2 bg-slate-900 text-orange-200 hover:bg-orange-200 hover:text-slate-900 hover:outline hover:outline-[1px] hover:outline-orange-200 hover:duration-200"
         style={{fontVariant: "small-caps"}}>
-        Explore Now
+          Explore Now
       </button>
     </form>
   )
