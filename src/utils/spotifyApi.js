f05b355218c0449979ce6633e9f9db738a7fa11d
Sarah-0911 <sarah.tabet69@gmail.com>
2025-02-03 18:22:14 +0100

    limit changed to 20

--- src/utils/spotifyApi.js
+++ src/utils/spotifyApi.js
@@ -1,6 +1,6 @@
 const fetchTracks = async (accessToken, userInput, offsetValue = 0) => {
   try {
-    const response = await fetch(`https://api.spotify.com/v1/search?q=${userInput}&type=track&limit=10&offset=${offsetValue}&include_external=audio`, {
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${userInput}&type=track&limit=20&offset=${offsetValue}&include_external=audio`, {
       headers: {
       Authorization: `Bearer ${accessToken}`
       }
